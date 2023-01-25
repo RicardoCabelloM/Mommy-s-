ab5842788d433c26341de63d19b1b5fd30326a89
RicardoCabello <richymontoya97@gmail.com>
2023-01-25 15:07:48 -0600

    se inicia JS

---
+++ Mommy's/js/main.js
@@ -0,0 +1,52 @@
+/*
+const nombre = prompt("Ingresa tu nombre")
+const nombre1 =prompt("Ingresa tu apellido")
+
+alert(nombre + " " + nombre1)
+
+console.log(nombre + " " + nombre1)
+
+const pastelFresa = (250)
+const payMango = (360)
+const resultado = pastelFresa + payMango
+console.log (resultado)
+*/
+
+/* Ejercicios - Condicionales*/
+ 
+/*
+alert ("Pastel de fresa $360");
+
+const pastelDeFresa = 360;
+
+const cantidadDePateles = prompt ("Solicita la cantidad de pasteles que desees")
+console.log ("$" + pastelDeFresa * cantidadDePateles)
+*/
+
+/*
+const anioActual = 2023;
+const edad = prompt("Ingresa tu edad")
+console.log (anioActual - edad)
+alert ("Bienvenido ser del año:" + (anioActual - edad))
+*/
+
+/*alert("Ingresa el pais de latinoamerica a donde desearias viajar")
+
+const pais= prompt("Ingresa un pais")
+
+let costo= 0;
+
+if(pais === "Colombia" || pais === "peru"){
+    costo= 40;
+} 
+else if(pais === "Ecuador" || pais === "Brasil"){
+    costo= 30;
+}
+else if(pais === "Mexico" || pais === "Estados Unidos"){
+    costo= 50;
+}
+
+console.log ("el costo del viaje es:" + " "+ "$" + costo)*/
+
+
+
