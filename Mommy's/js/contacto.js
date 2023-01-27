b6ae267ff7b3bacc1ad1e5cabb2ced734b3b5569
RicardoCabello <richymontoya97@gmail.com>
2023-01-26 22:45:39 -0600

    Primera entrega JS

---
+++ Mommy's/js/contacto.js
@@ -0,0 +1,10 @@
+let costo= 0 
+
+for(let i = 0; i < 3; i ++) {
+    let nombre= prompt("Ingrese su nombre");
+    let telefono= prompt("Ingrese su telefono")
+    let correoElectronico= prompt("Ingrese su correo electronico")
+
+    console.log(nombre + telefono + correoElectronico)
+}
+
--- Mommy's/js/inicio.js
+++ Mommy's/js/inicio.js
@@ -49,4 +49,41 @@ else if(pais === "Mexico" || pais === "Estados Unidos"){
 console.log ("el costo del viaje es:" + " "+ "$" + costo)*/
 
 
+/*const nombre= prompt("Ingresa un nombre:")
+let edad= prompt("Ingresa una edad:");
+
+if(nombre === "Gabriel" && edad >= 24 ||nombre === "Maria" && (edad >= 28 && edad <= 35)){
+    console.log("Mi nombre es: " + nombre)
+    console.log("Mi edad es: " + edad)
+}  else alert("No eres Gabriel o Maria")*/
+
+/*if(nombre === "Maria" && (edad >= 28 && edad <= 35)){
+    console.log("Mi nombre es " + nombre)
+    console.log("Mi edad es: " + edad) 
+} else if (alert("No eres Maria"));*/
+
+
+
+
+
+/*INDEX*/
+
+let costo= 0 
+
+let pastel= prompt("Ingresa el nombre de los pasteles desees ordenar (El precio no incluye IVA 16%, escribe Salir para dejar de ordenar");
+
+if (pastel === "Pay de fresa"){
+    costo= 500;
+} else if (pastel === "Pay de mango"){
+    costo= 270;
+} else if (pastel === "Pay de fresa mediano"){
+    costo= 150;
+} else if (pastel === "Pastel  cruz   azul" || precioPastel === "Pastel infantil" || pastel === "Pastel personalizado"){
+    costo= 400;
+} 
+
+let costoTotal= (costo*16/100+costo);
+alert ("Total a pagar= $" + costoTotal);
+
+/* 16x400/100*/
 
